0959360ecb9e7b1ce54e71aca354bd4cbab13975
Siarhei Piatselski <Siarhei_Piatselski@epam.com>
2022-06-20 15:18:35 +0300

    Add #4 Exception_Handling

---
+++ 4_Exception_Handling/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+             static void WriteFirstCharacter(string inputLine)
+             {
+                if (string.IsNullOrEmpty(inputLine))
+                {
+                    throw new ArgumentException("String must not be empty or null");
+                }
+                Console.WriteLine(inputLine[0]);
+             }
+
+             try
+             {
+                 WriteFirstCharacter(string.Empty);
+             }
+             catch (ArgumentException e)
+             {
+                 Console.WriteLine($"Incorrect input string.{e.Message}");
+             }
+             Console.ReadKey();
+        }
+    }
+}
---
+++ 4_Exception_Handling/Task3/Exceptions/UserTaskServiceException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task3.Exceptions
+{
+    [Serializable]
+    public class UserTaskServiceException : Exception
+    {
+        public UserTaskServiceException()
+        {
+        }
+
+        public UserTaskServiceException(string message) : base(message)
+        {
+        }
+
+        public UserTaskServiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserTaskServiceException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
